0364b5d90aace7c1b4ed01ab98562846acaf2cb7
BaffIrodov <sergej.davidyuk@yandex.ru>
2024-05-17 19:39:19 +0300

    fix

---
+++ src/main/java/application/cellaction/ActionSayAround.java
@@ -0,0 +1,17 @@
+package application.cellaction;
+
+import application.Square;
+import application.cell.Cell;
+
+import static application.Main.energyCostSettings;
+
+public class ActionSayAround extends CellAction {
+
+    {
+        this.activeAction = false;
+    }
+
+    @Override
+    public void execute(Cell cell, Square currentSquare) {
+    }
+}
